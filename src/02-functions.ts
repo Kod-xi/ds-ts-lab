7f2b1bd5cbd8a29ddb25eba185a570a711518bbb
Kod-xi <kian.j.odea@gmail.com>
2026-09-13 12:56:30 +0100

    Typed function signature

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,2 +1,36 @@
 import {Friend, Colleague } from './myTypes'
+import { friends } from './01-basics'
+import { colleagues } from './01-basics'
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+function addColleague(cs: Colleague[], n: string, d: string, em: string): Colleague[] {
+    const newC: Colleague = {
+        name: n,
+        department: d,
+        contact: {
+            email: em,
+            extension: highestExtension(cs).contact.extension + 1,
+        },
+    };
+    cs.push(newC);
+    return cs;
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
